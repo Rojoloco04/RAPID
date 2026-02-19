f8bdf1571c66a1f8e029ea73343afd471b4c347e
Rojoloco04 <parrackjack@gmail.com>
2026-02-19 11:55:15 -0600

    cleanup thousands of vivado build artifacts (this is dangerous) let's see if it works - added to gitignore

--- RAPID/_ide/workspace_journal_11236.py
+++
@@ -1,8 +0,0 @@
-# 2026-02-03T14:35:49.690097500
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="RAPID")
-
-vitis.dispose()
-
--- RAPID/_ide/workspace_journal_11540.py
+++
@@ -1,8 +0,0 @@
-# 2025-12-05T12:18:54.001312700
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="RAPID")
-
-vitis.dispose()
-
